0a562ec82992accd7129628188de26c9cfae71c0
thylakoids <liyulongsjtu@hotmail.com>
2017-08-22 16:34:29 +0800

    add new route, refresh ok

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask,template_rendered
+from flask import Flask,render_template
 
-app = Flask(__name__, static_folder='dist')
-
+app = Flask(__name__, template_folder='dist',static_folder='dist',static_url_path='')
 @app.route('/')
 def index():
-    return template_rendered('index.html')
+    return render_template('index.html')
+@app.route('/<path>')
+def indexp(path):
+    return render_template('index.html')
+
 '''
 @app.route('/<path:path>')
 def static_proxy(path):
     # send_static_file will guess the correct MIME type
     return app.send_static_file(path)'''
 if __name__=='__main__':
-	app.run()
+	app.run(debug=True)
