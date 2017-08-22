8a8aff5da6148d55c4bc2b28d858ec642250e982
thylakoids <liyulongsjtu@hotmail.com>
2017-08-22 23:48:23 +0800

    solve conflit

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def static_proxy(path):
     return app.send_static_file(path)
 '''
 =======
-    return app.send_static_file(path)'''
 >>>>>>> master
 if __name__=='__main__':
 	app.run(debug=True)
