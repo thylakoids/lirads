0c3c71c6d61baf4f7d34eb952f048bd9c27fcfa7
thylakoids <liyulongsjtu@hotmail.com>
2017-08-23 00:27:34 +0800

    flask route ok (build -d /dist/)

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask,template_rendered
+from flask import Flask,render_template
 
-app = Flask(__name__, static_folder='dist')
+app = Flask(__name__, static_folder='dist',static_url_path='/dist',template_folder='dist')
 
+@app.route('/<any>')
 @app.route('/')
-def index():
-    return template_rendered('index.html')
+def index(any=None):
+    return render_template('index.html')
 '''
 @app.route('/<path:path>')
 def static_proxy(path):
     # send_static_file will guess the correct MIME type
     return app.send_static_file(path)'''
 if __name__=='__main__':
-	app.run()
+	app.run(debug=True)
